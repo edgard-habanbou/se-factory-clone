ac4f6507a205c18899f8cd771e82e34fd48b1e4f
edgard <edgard.habanbou@hotmail.com>
2023-11-17 01:38:27 +0200

    added easter egg section (hiring portals) PS:only shows on mobile version

--- scripts/style.js
+++ scripts/style.js
@@ -1,17 +1,32 @@
-// function to make the icons smaller when on a small device
 function handleMediaChange(mq) {
   const icons = document.querySelectorAll("i");
+  const easteregg = document.getElementById("easterEgg");
 
   if (mq.matches) {
     icons.forEach((icon) => {
       icon.classList.remove("fa-2x");
     });
+    if (mq.media == "(max-width: 550px)") {
+      easteregg.classList.remove("hidden");
+    }
   } else {
     icons.forEach((icon) => {
       icon.classList.add("fa-2x");
     });
+    if (mq.media == "(max-width: 550px)") {
+      easteregg.classList.add("hidden");
+    }
   }
 }
-const mediaQuery = window.matchMedia("(max-width: 991px)");
-mediaQuery.addListener(handleMediaChange);
-handleMediaChange(mediaQuery);
+
+// Create media queries
+const mediaQuery550 = window.matchMedia("(max-width: 550px)");
+const mediaQuery991 = window.matchMedia("(max-width: 991px)");
+
+// Initial calls to set the initial state
+handleMediaChange(mediaQuery550);
+handleMediaChange(mediaQuery991);
+
+// Add event listeners to handle changes
+mediaQuery550.addListener(() => handleMediaChange(mediaQuery550));
+mediaQuery991.addListener(() => handleMediaChange(mediaQuery991));
