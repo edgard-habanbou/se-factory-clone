26b4529289bd6cf1f6989b6adbc5131662b7f1ea
edgard <edgard.habanbou@hotmail.com>
2023-11-16 22:51:51 +0200

    added section 8 (Contact form) with styles

--- scripts/script.js
+++ scripts/script.js
@@ -87,10 +87,10 @@ const showSlides = () => {
     slideIndex = 1;
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" dot-active", "");
   }
   slides[slideIndex - 1].style.display = "flex";
-  dots[slideIndex - 1].className += " active";
+  dots[slideIndex - 1].className += " dot-active";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 };
 const showTab = (tabId) => {
